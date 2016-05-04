3fdbdee91fade341717b5730df0d65f4053a688c
Tom W Hartung <tomwhartung@gmail.com>
2016-05-03 23:49:31 -0600

    Added variables for ledPin and delayMillisecs.

--- my_examples/01.Basics/_01-Blink/_01-Blink.ino
+++ my_examples/01.Basics/_01-Blink/_01-Blink.ino
@@ -13,17 +13,19 @@
   by Scott Fitzgerald
  */
 
+int ledPin = 13;
+int delayMillisecs = 1000;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);              // wait for a second
+  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                  // wait for a second
+  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                  // wait for a second
 }
