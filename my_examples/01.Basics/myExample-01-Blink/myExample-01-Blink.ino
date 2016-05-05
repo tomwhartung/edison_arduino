124c7a124a275529f37ab277441571d61d73d226
Tom W Hartung <tomwhartung@gmail.com>
2016-05-05 10:44:55 -0600

    Updated to use the led to flash the letter "A" (for Arduino) in Morse Code, by adding functions for dots and dashes, so we can easily play around with this more if we want to later.

--- my_examples/01.Basics/myExample-01-Blink/myExample-01-Blink.ino
+++ my_examples/01.Basics/myExample-01-Blink/myExample-01-Blink.ino
@@ -13,9 +13,15 @@
   by Scott Fitzgerald
  */
 
+/*
+ * Morse Code timing Reference: 
+ *   https://en.wikipedia.org/wiki/Morse_code#Representation.2C_timing_and_speeds
+ */
 int ledPin = 13;
-int onMilliseconds = 200;
-int offMilliseconds = 800;
+int dotMs = 200;
+int dashMs = 3 * dotMs;
+int elementGapMs = dotMs;
+int letterGapMs  = dashMs;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -23,12 +29,32 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   Serial.begin( 9600 );
 }
-
+//
 // the loop function runs over and over again forever
+// An "A" (for Arduino) in Morse Code is "dot dash"
+//
 void loop() {
-  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(onMilliseconds);        // wait for the specified number of milliseconds
-  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
-  delay(offMilliseconds);       // wait for the specified number of milliseconds
+  dot();
+  delay(elementGapMs);   // leave it off for the desired gap between dashes and dots in a letter
+  dash();
+  delay(letterGapMs);    // leave it off for the desired gap between letters
   Serial.println( "in loop fcn ledPin = " + ledPin );
 }
+
+//
+// Turn the led on for dot milliseconds, then turn it off
+//
+void dot() {
+  digitalWrite(ledPin, HIGH);     // turn the LED on (HIGH is the voltage level)
+  delay(dotMs);                   // wait for "dot" number of milliseconds
+  digitalWrite(ledPin, LOW);      // turn the LED off by making the voltage LOW
+}
+//
+// Turn the led on for dash milliseconds, then turn it off
+//
+void dash() {
+  digitalWrite(ledPin, HIGH);     // turn the LED on (HIGH is the voltage level)
+  delay(dashMs);                  // wait for "dash" number of milliseconds
+  digitalWrite(ledPin, LOW);      // turn the LED off by making the voltage LOW
+}
+
