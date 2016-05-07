5d6998073612738621eff4a24516ee2931df49b8
Tom W Hartung <tomwhartung@gmail.com>
2016-05-06 23:26:12 -0600

    Updated to blink out A-R-D-U-I-N-O .

--- my_examples/01.Basics/myExample-01-Blink/myExample-01-Blink.ino
+++ my_examples/01.Basics/myExample-01-Blink/myExample-01-Blink.ino
@@ -23,25 +23,8 @@ int dashMs = 3 * dotMs;
 int elementGapMs = dotMs;
 int letterGapMs  = dashMs;
 //
-// the setup function runs once when you press reset or power the board
-//
-void setup() {
-  // initialize digital pin 13 as an output.
-  pinMode(ledPin, OUTPUT);
-  // Serial.begin( 9600 );
-}
-//
-// the loop function runs over and over again forever
-// An "A" (for Arduino) in Morse Code is "dot dash"
-//
-void loop() {
-  dot();
-  delay(elementGapMs);   // leave it off for the desired gap between dashes and dots in a letter
-  dash();
-  delay(letterGapMs);    // leave it off for the desired gap between letters
-  // Serial.println( "in loop fcn ledPin = " + ledPin );
-}
-
+// General Functions
+// -----------------
 //
 // Turn the led on for dot milliseconds, then turn it off
 //
@@ -57,5 +40,86 @@ void dash() {
   digitalWrite(ledPin, HIGH);     // turn the LED on (HIGH is the voltage level)
   delay(dashMs);                  // wait for "dash" number of milliseconds
   digitalWrite(ledPin, LOW);      // turn the LED off by making the voltage LOW
+}
+//
+// Functions for specific letters
+// ------------------------------
+//
+void blinkLetterA() {
+  dot();
+  delay(elementGapMs);   // leave it off for the desired gap between dashes and dots in a letter
+  dash();
+  delay(letterGapMs);    // leave it off for the desired gap between letters
+}
+void blinkLetterR() {
+  dot();
+  delay(elementGapMs);   // leave it off for the desired gap between dashes and dots in a letter
+  dash();
+  delay(elementGapMs);   // leave it off for the desired gap between dashes and dots in a letter
+  dot();
+  delay(letterGapMs);    // leave it off for the desired gap between letters
+}
+void blinkLetterD() {
+  dash();
+  delay(elementGapMs);   // leave it off for the desired gap between dashes and dots in a letter
+  dot();
+  delay(elementGapMs);   // leave it off for the desired gap between dashes and dots in a letter
+  dot();
+  delay(letterGapMs);    // leave it off for the desired gap between letters
+}
+void blinkLetterU() {
+  dot();
+  delay(elementGapMs);   // leave it off for the desired gap between dashes and dots in a letter
+  dot();
+  delay(elementGapMs);   // leave it off for the desired gap between dashes and dots in a letter
+  dash();
+  delay(letterGapMs);    // leave it off for the desired gap between letters
+}
+void blinkLetterI() {
+  dot();
+  delay(elementGapMs);   // leave it off for the desired gap between dashes and dots in a letter
+  dot();
+  delay(letterGapMs);    // leave it off for the desired gap between letters
+}
+void blinkLetterN() {
+  dash();
+  delay(elementGapMs);   // leave it off for the desired gap between dashes and dots in a letter
+  dot();
+  delay(letterGapMs);    // leave it off for the desired gap between letters
+}
+void blinkLetterO() {
+  dash();
+  delay(elementGapMs);   // leave it off for the desired gap between dashes and dots in a letter
+  dash();
+  delay(elementGapMs);   // leave it off for the desired gap between dashes and dots in a letter
+  dash();
+  delay(letterGapMs);    // leave it off for the desired gap between letters
+}
+//
+// "mainline:" setup and loop functions
+// ------------------------------------
+//
+// the setup function runs once when you press reset or power the board
+//
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(ledPin, OUTPUT);
+  // Serial.begin( 9600 );
+}
+//
+// the loop function runs over and over again forever
+// Arduino) in Morse Code is
+// A          R              D              U              I         N          O
+// dot dash   dot dash dot   dash dot dot   dot dot dash   dot dot   dash dot   dash dash dash
+//
+void loop() {
+  blinkLetterA();
+  blinkLetterR();
+  blinkLetterD();
+  blinkLetterU();
+  blinkLetterI();
+  blinkLetterN();
+  blinkLetterO();
+  // Serial.println( "in loop fcn ledPin = " + ledPin );
 }
 
