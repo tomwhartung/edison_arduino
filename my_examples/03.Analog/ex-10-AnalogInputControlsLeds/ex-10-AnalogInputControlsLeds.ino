5984acddd2d14b6426ca4416d5ea05f4d90c99ae
Tom W Hartung <tomwhartung@gmail.com>
2016-05-17 10:06:09 -0600

    Comment cleanup in ex-10-AnalogInputControlsLeds/ex-10-AnalogInputControlsLeds.ino before trying to copy it and make a PWM version like we did with python.  The next few days are extremely busy with interviews and google i/o etc. so we will fit in some work when we can.

--- my_examples/03.Analog/ex-10-AnalogInputControlsLeds/ex-10-AnalogInputControlsLeds.ino
+++ my_examples/03.Analog/ex-10-AnalogInputControlsLeds/ex-10-AnalogInputControlsLeds.ino
@@ -1,56 +1,48 @@
 /*
-  Analog Input
- Demonstrates analog input by reading an analog sensor on analog pin 0 and
- turning on and off a light emitting diode(LED)  connected to digital pin 13.
- The amount of time the LED will be on and off depends on
- the value obtained by analogRead().
-
- The circuit:
- * Potentiometer attached to analog input 0
- * center pin of the potentiometer to the analog pin
- * one side pin (either one) to ground
- * the other side pin to +5V
- * LED anode (long leg) attached to digital output 13
- * LED cathode (short leg) attached to ground
-
- * Note: because most Arduinos have a built-in LED attached
- to pin 13 on the board, the LED is optional.
-
-
- Created by David Cuartielles
- modified 30 Aug 2011
- By Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/AnalogInput
-
+ *  Analog Input
+ * Demonstrates analog input by reading an analog sensor on analog pin 0 and
+ * turning on and off light emitting diodes (LEDs) connected to digital pins 3 and 4.
+ * The amount of time the LED will be on and off depends on
+ * the value obtained by analogRead().
+ *
+ * The circuit:
+ * - Potentiometer attached to analog input 0
+ * - LED attached to digital output 3
+ *
+ * Created by David Cuartielles
+ * Modified 30 Aug 2011 By Tom Igoe
+ * Modified 17 May 2016 By Tom Hartung
+ *
+ * This example code is in the public domain.
+ * http://www.arduino.cc/en/Tutorial/AnalogInput
  */
 
-int sensorPin = A0;    // select the input pin for the potentiometer
+int sensorPin = A0;   // select the input pin for the potentiometer
 int sensorValue = 0;  // variable to store the value coming from the sensor
 int ledPin3 = 3;      // the LED on pin 3
 int ledPin4 = 4;      // the LED on pin 4
 
+/**
+ * declare the ledPins as OUTPUT
+ */
 void setup() {
-  // declare the ledPin as an OUTPUT:
   pinMode(ledPin3, OUTPUT);
   pinMode(ledPin4, OUTPUT);
   Serial.begin( 9600 );
 }
 
+/**
+ * read the value from the sensor:
+ * turn the ledPins on and stop the program for <sensorValue> milliseconds:
+ * turn the ledPin off and stop the program for for <sensorValue> milliseconds:
+ */
 void loop() {
-  // read the value from the sensor:
   sensorValue = analogRead(sensorPin);
   Serial.println( sensorValue );
-  // turn the ledPin on
   digitalWrite(ledPin3, HIGH);
   digitalWrite(ledPin4, LOW);
-  // stop the program for <sensorValue> milliseconds:
   delay(sensorValue);
-  // turn the ledPin off:
   digitalWrite(ledPin3, LOW);
   digitalWrite(ledPin4, HIGH);
-  // stop the program for for <sensorValue> milliseconds:
   delay(sensorValue);
 }
