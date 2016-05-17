2ab144d38287ce001830a14c6d5b211f2ed0569e
Tom W Hartung <tomwhartung@gmail.com>
2016-05-16 23:07:02 -0600

    Fixed comments in ex-08-buttonTurnsOnLed.ino prior to using it as a starting point to create ex-18....

--- my_examples/02.Digital/ex-08-buttonTurnsOnLed/ex-08-buttonTurnsOnLed.ino
+++ my_examples/02.Digital/ex-08-buttonTurnsOnLed/ex-08-buttonTurnsOnLed.ino
@@ -1,33 +1,23 @@
 /*
-  Button
-
- Turns on and off a light emitting diode(LED) connected to digital
- pin 13, when pressing a pushbutton attached to pin 2.
-
-
- The circuit:
- * LED attached from pin 13 to ground
- * pushbutton attached to pin 2 from +5V
- * 10K resistor attached to pin 2 from ground
-
- * Note: on most Arduinos there is already an LED on the board
- attached to pin 13.
-
-
- created 2005
- by DojoDave <http://www.0j0.org>
- modified 30 Aug 2011
- by Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/Button
+ * Button Turns on and off a light emitting diode(LED) connected to digital
+ * pin 3, when pressing a pushbutton attached to pin 8.
+ *
+ * The circuit:
+ * - Pushbutton attached to pin 8
+ * - LED attached on pin 3
+ *
+ * created 2005 by DojoDave <http://www.0j0.org>
+ * modified 30 Aug 2011 by Tom Igoe
+ * modified 16 May 2016 by Tom Hartung
+ *
+ * This example code is in the public domain.
+ * http://www.arduino.cc/en/Tutorial/Button
  */
-
-// constants won't change. They're used here to
+//
 // set pin numbers:
-const int buttonPin = 8;     // the number of the pushbutton pin
-const int ledPin4   = 4;     // the number of the LED pin
+//
+const int buttonPin = 8;     // pushbutton pin number
+const int ledPin4   = 3;     // LED pin number
 
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
